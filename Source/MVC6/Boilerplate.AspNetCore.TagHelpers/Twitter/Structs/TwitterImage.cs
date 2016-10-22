ad748deabfb5ea238fc87131809cf03209d8ea1d
VeXHarbinger <alexpearlstein@gmail.com>
2016-10-22 15:25:32 -0400

    Revert "Added Intellesense and ImageAlt"
    This reverts commit a559efdde38a5fb03b5a61adaf6fcb31d7a50d3b.

--- Source/MVC6/Boilerplate.AspNetCore.TagHelpers/Twitter/Structs/TwitterImage.cs
+++ Source/MVC6/Boilerplate.AspNetCore.TagHelpers/Twitter/Structs/TwitterImage.cs
@@ -7,17 +7,10 @@
     /// </summary>
     public class TwitterImage
     {
-        #region Public Constructors
-
         /// <summary>
         /// Initializes a new instance of the <see cref="TwitterImage"/> class.
         /// </summary>
-        /// <param name="imageUrl">
-        /// The image URL. The Image must be less than 1MB in size.
-        /// </param>
-        /// <exception cref="System.ArgumentNullException">
-        /// imageUrl
-        /// </exception>
+        /// <param name="imageUrl">The image URL. The Image must be less than 1MB in size.</param>
         public TwitterImage(string imageUrl)
         {
             if (imageUrl == null)
@@ -31,62 +24,33 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="TwitterImage"/> class.
         /// </summary>
-        /// <param name="imageUrl">
-        /// The image URL. The Image must be less than 1MB in size.
-        /// </param>
-        /// <param name="width">
-        /// The width of the image in pixels to help Twitter accurately preserve the aspect ratio of the image when resizing. This property is optional.
-        /// </param>
-        /// <param name="height">
-        /// The height of the image in pixels to help Twitter accurately preserve the aspect ratio of the image when resizing. This property is optional.
-        /// </param>
-        /// <param name="imageAlt">
-        /// The text description of the image conveying the essential nature of an image to users who are visually impaired.
-        /// </param>
-        public TwitterImage(string imageUrl, int width, int height, string imageAlt)
+        /// <param name="imageUrl">The image URL. The Image must be less than 1MB in size.</param>
+        /// <param name="width">The width of the image in pixels to help Twitter accurately preserve the aspect ratio
+        /// of the image when resizing. This property is optional.</param>
+        /// <param name="height">The height of the image in pixels to help Twitter accurately preserve the aspect ratio
+        /// of the image when resizing. This property is optional.</param>
+        public TwitterImage(string imageUrl, int width, int height)
             : this(imageUrl)
         {
             this.Height = height;
             this.Width = width;
-            this.ImageAlt = imageAlt;
         }
 
-        #endregion Public Constructors
-
-        #region Public Properties
-
         /// <summary>
-        /// Gets or sets the height of the image in pixels to help Twitter accurately preserve the aspect ratio of the image when resizing. This property is optional.
+        /// Gets or sets the height of the image in pixels to help Twitter accurately preserve the aspect ratio of
+        /// the image when resizing. This property is optional.
         /// </summary>
-        /// <value>
-        /// The height of the image in pixels.
-        /// </value>
         public int? Height { get; set; }
-
-        /// <summary>
-        /// Gets the image alt.
-        /// </summary>
-        /// <value>
-        /// A text description of the image conveying the essential nature of an image to users who are visually impaired.
-        /// </value>
-        public string ImageAlt { get; }
 
         /// <summary>
         /// Gets the image URL. The Image must be less than 1MB in size.
         /// </summary>
-        /// <value>
-        /// The image URL. The Image must be less than 1MB in size.
-        /// </value>
         public string ImageUrl { get; }
 
         /// <summary>
-        /// Gets or sets the width of the image in pixels to help Twitter accurately preserve the aspect ratio of the image when resizing. This property is optional.
+        /// Gets or sets the width of the image in pixels to help Twitter accurately preserve the aspect ratio of
+        /// the image when resizing. This property is optional.
         /// </summary>
-        /// <value>
-        /// The width of the image in pixels.
-        /// </value>
         public int? Width { get; set; }
-
-        #endregion Public Properties
     }
 }
