5f7a3837e1478ca1ab3dda24a29ca53f14b1ea75
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2017-02-10 10:26:03 +0000

    Simplify AssemblyInfo

--- Source/MVC6/Boilerplate.AspNetCore/Properties/AssemblyInfo.cs
+++ Source/MVC6/Boilerplate.AspNetCore/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Boilerplate.AspNetCore")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Boilerplate.AspNetCore")]
-[assembly: AssemblyCopyright("Copyright © Muhammad Rehan Saeed 2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
