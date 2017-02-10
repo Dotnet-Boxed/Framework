e356989fad11df74a291c3d050635241d5970e9c
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2017-02-10 10:25:16 +0000

    Added HttpHeader, RequestIdHttpHeader and UserAgentHttpHeader MVC filter attributes.

---
+++ Source/MVC6/Boilerplate.AspNetCore/Filters/RequestIdHttpHeaderAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace Framework.AspNetCore.Filters
+{
+    using System;
+    using System.Linq;
+    using Microsoft.Extensions.Primitives;
+
+    /// <summary>
+    /// Require the X-Request-ID HTTP header to be specified in a request optionally forward it in the response.
+    /// </summary>
+    /// <seealso cref="Framework.AspNetCore.Filters.HttpHeaderAttribute" />
+    public class RequestIdHttpHeaderAttribute : HttpHeaderAttribute
+    {
+        private const string RequestIdHttpHeaderName = "X-Request-ID";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestIdHttpHeaderAttribute"/> class.
+        /// </summary>
+        public RequestIdHttpHeaderAttribute()
+            : base(RequestIdHttpHeaderName)
+        {
+        }
+
+        /// <summary>
+        /// Returns <c>true</c> if the X-Request-ID HTTP header contains a GUID, otherwise <c>false</c>.
+        /// </summary>
+        /// <param name="headerValues">The header values.</param>
+        /// <returns><c>true</c> if the X-Request-ID HTTP header values are valid; otherwise, <c>false</c>.</returns>
+        public override bool IsValid(StringValues headerValues)
+        {
+            Guid guid;
+            return !StringValues.IsNullOrEmpty(headerValues) &&
+                headerValues.All(x => Guid.TryParse(x, out guid));
+        }
+    }
+}
---
+++ Source/MVC6/Boilerplate.AspNetCore/Filters/UserAgentHttpHeaderAttribute.cs
@@ -0,0 +1,95 @@
+﻿namespace Framework.AspNetCore.Filters
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http.Headers;
+    using Microsoft.Extensions.Primitives;
+
+    /// <summary>
+    /// Require the User-Agent HTTP header to be specified in a request.
+    /// </summary>
+    /// <seealso cref="Framework.AspNetCore.Filters.HttpHeaderAttribute" />
+    public class UserAgentHttpHeaderAttribute : HttpHeaderAttribute
+    {
+        private const string UserAgentHttpHeaderName = "User-Agent";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserAgentHttpHeaderAttribute"/> class.
+        /// </summary>
+        public UserAgentHttpHeaderAttribute()
+            : base(UserAgentHttpHeaderName)
+        {
+        }
+
+        /// <summary>
+        /// Returns <c>true</c> if the User-Agent HTTP header value is valid, otherwise returns <c>false</c>.
+        /// </summary>
+        /// <param name="headerValues">The header values.</param>
+        /// <returns><c>true</c> if the User-Agent HTTP header values are valid; otherwise, <c>false</c>.</returns>
+        public override bool IsValid(StringValues headerValues)
+        {
+            var isValid = false;
+
+            if (!StringValues.IsNullOrEmpty(headerValues))
+            {
+                var values = headerValues
+                    .SelectMany(x => Split(x))
+                    .Select(
+                        x =>
+                        {
+                            ProductInfoHeaderValue productInfo;
+                            var parsed = ProductInfoHeaderValue.TryParse(x, out productInfo);
+                            return new
+                            {
+                                Parsed = parsed,
+                                ProductInfo = productInfo
+                            };
+                        })
+                    .ToArray();
+
+                isValid = values.All(x => x.Parsed) &&
+                    values
+                        .Where(x => x.ProductInfo.Product != null)
+                        .All(x => !string.IsNullOrWhiteSpace(x.ProductInfo.Product.Name) && !string.IsNullOrWhiteSpace(x.ProductInfo.Product.Version));
+            }
+
+            return isValid;
+        }
+
+        private static IEnumerable<string> Split(string value)
+        {
+            var inComment = false;
+            var startIndex = 0;
+            for (int i = 0; i < value.Length; ++i)
+            {
+                var c = value[i];
+
+                switch (c)
+                {
+                    case '(':
+                        inComment = true;
+                        break;
+                    case ')':
+                        inComment = false;
+                        break;
+                    case ' ':
+                        if (!inComment)
+                        {
+                            var subValue = value.Substring(startIndex, i - startIndex);
+                            startIndex = i;
+                            yield return subValue;
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+
+                if (i == value.Length - 1)
+                {
+                    yield return value.Substring(startIndex, i - startIndex + 1);
+                }
+            }
+        }
+    }
+}
