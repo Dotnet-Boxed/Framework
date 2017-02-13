fc278b17b1c86472f70b6479b8531959c9bab7dc
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2017-02-13 16:58:21 +0000

    Fix build

--- Tests/MVC6/Boilerplate.Test/AsyncTranslatorTest.cs
+++ Tests/MVC6/Boilerplate.Test/AsyncTranslatorTest.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Threading.Tasks;
-    using Framework;
+    using Boilerplate;
     using Xunit;
 
     public class AsyncTranslatorTest
