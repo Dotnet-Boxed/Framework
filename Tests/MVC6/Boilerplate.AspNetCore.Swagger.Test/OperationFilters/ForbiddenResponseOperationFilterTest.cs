d8ff93a065f8de1cc4dcc1e1b05dd51f98545790
Muhammad Rehan Saeed <RehanSaeed@users.noreply.github.com>
2017-05-16 16:30:08 +0100

    Only add 401 and 403 response codes if they have not already been added.

--- Tests/MVC6/Boilerplate.AspNetCore.Swagger.Test/OperationFilters/ForbiddenResponseOperationFilterTest.cs
+++ Tests/MVC6/Boilerplate.AspNetCore.Swagger.Test/OperationFilters/ForbiddenResponseOperationFilterTest.cs
@@ -77,6 +77,17 @@
             Assert.Empty(this.operation.Responses);
         }
 
+        [Fact]
+        public void Apply_AlreadyHasForbiddenResponse_DoesNothing()
+        {
+            var response = new Response();
+            this.operation.Responses.Add("403", response);
+            this.operationFilter.Apply(this.operation, this.operationFilterContext);
+
+            Assert.Equal(1, this.operation.Responses.Count);
+            Assert.Same(response, this.operation.Responses["403"]);
+        }
+
         private void Apply_HasRequirement_Adds403Response(IAuthorizationRequirement authorizationRequirement)
         {
             var requirements = new List<IAuthorizationRequirement>() { authorizationRequirement };
