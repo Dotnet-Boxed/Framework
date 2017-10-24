f8a9e0454aa991a953fae7a3f0910ed68986ae19
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2017-10-24 09:27:01 +0100

    Fix user agent string validation

--- Tests/Boilerplate.AspNetCore.Test/Filters/UserAgentHttpHeaderAttributeTest.cs
+++ Tests/Boilerplate.AspNetCore.Test/Filters/UserAgentHttpHeaderAttributeTest.cs
@@ -16,6 +16,8 @@ namespace Boilerplate.AspNetCore.Test.Filters
         [InlineData("Application Name/1.0.0.0")]
         [InlineData("ApplicationName/1.0.0.0")]
         [InlineData("Application-Name/1.0.0.0")]
+        [InlineData("ApplicationName/1.0.0.0,(OS Name 1.0.0.0)")]
+        [InlineData("Application-Name/1.0.0.0,(OS Name 1.0.0.0)")]
         [InlineData("ApplicationName/1.0.0.0 (OS Name 1.0.0.0)")]
         [InlineData("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36")]
         public void IsValid_ValidUserAgent_ReturnsTrue(string headerValue)
@@ -28,8 +30,6 @@ namespace Boilerplate.AspNetCore.Test.Filters
         [Theory]
         [InlineData(null)]
         [InlineData("")]
-        [InlineData("ApplicationName/1.0.0.0,(OS Name 1.0.0.0)")]
-        [InlineData("Application-Name/1.0.0.0,(OS Name 1.0.0.0)")]
         public void IsValid_InvalidUserAgent_ReturnsFalse(string headerValue)
         {
             bool isValid = this.filter.IsValid(GetStringValues(headerValue));
