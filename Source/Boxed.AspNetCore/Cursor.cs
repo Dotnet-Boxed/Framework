4cd18aaee6d4a7f27777828689cb0857b21ab1f7
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2019-02-13 10:21:23 +0000

    Add Cursor helper methods

---
+++ Source/Boxed.AspNetCore/Cursor.cs
@@ -0,0 +1,93 @@
+namespace Boxed.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Reads or writes cursors which are opaque strings representing a position in a list of items.
+    /// </summary>
+    public static class Cursor
+    {
+        /// <summary>
+        /// Gets the strongly typed <typeparamref name="T"/> from the specified <paramref name="cursor"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the cursor value.</typeparam>
+        /// <param name="cursor">The cursor.</param>
+        /// <returns>The cursor value.</returns>
+        public static T FromCursor<T>(string cursor)
+        {
+            if (string.IsNullOrEmpty(cursor))
+            {
+                return default;
+            }
+
+            string decodedValue;
+            try
+            {
+                decodedValue = Base64Decode(cursor);
+            }
+            catch (FormatException)
+            {
+                return default;
+            }
+
+            var type = typeof(T);
+            type = Nullable.GetUnderlyingType(type) ?? type;
+
+            return (T)Convert.ChangeType(decodedValue, type, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Gets the cursor for the first and last item in the collection.
+        /// </summary>
+        /// <typeparam name="TItem">The type of the items in the collection.</typeparam>
+        /// <typeparam name="TCursor">The type of the cursor value.</typeparam>
+        /// <param name="enumerable">The enumerable.</param>
+        /// <param name="getCursorProperty">The get cursor property.</param>
+        /// <returns>The first and last cursor in the collection.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="getCursorProperty"/> is <c>null</c>.</exception>
+        public static (string firstCursor, string lastCursor) GetFirstAndLastCursor<TItem, TCursor>(
+            IEnumerable<TItem> enumerable,
+            Func<TItem, TCursor> getCursorProperty)
+        {
+            if (getCursorProperty == null)
+            {
+                throw new ArgumentNullException(nameof(getCursorProperty));
+            }
+
+            if (enumerable == null || enumerable.Count() == 0)
+            {
+                return (null, null);
+            }
+
+            var firstCursor = ToCursor(getCursorProperty(enumerable.First()));
+            var lastCursor = ToCursor(getCursorProperty(enumerable.Last()));
+
+            return (firstCursor, lastCursor);
+        }
+
+        /// <summary>
+        /// Gets the cursor from the strongly typed <paramref name="value"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the cursor.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>The cursor.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
+        public static string ToCursor<T>(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return Base64Encode(value.ToString());
+        }
+
+        private static string Base64Decode(string value) => Encoding.UTF8.GetString(Convert.FromBase64String(value));
+
+        private static string Base64Encode(string value) => Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+    }
+}
