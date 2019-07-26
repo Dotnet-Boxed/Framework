ac127c8b9f5b174752465a735788358885edca29
Victorio Berra <toryberra@gmail.com>
2019-07-26 08:52:10 -0500

    Remove GraphQL specific code

--- Source/Boxed.DotnetNewTest/Models/GraphQLResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Boxed.Templates.FunctionalTest.Models
-{
-    using System.Collections.Generic;
-
-    public class GraphQLResponse
-    {
-        public List<GraphQLError> Errors { get; set; }
-    }
-}
