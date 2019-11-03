36f7ff36e3e257306b8894124fd55e9236e61b7f
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2019-11-03 14:36:18 +0000

    Implement basic problem details schema filter

--- Source/Boxed.AspNetCore.Swagger/SchemaFilters/ProblemDetailsSchemaFilter.cs
+++ Source/Boxed.AspNetCore.Swagger/SchemaFilters/ProblemDetailsSchemaFilter.cs
@@ -1,6 +1,7 @@
 namespace Boxed.AspNetCore.Swagger.SchemaFilters
 {
     using System;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.OpenApi.Any;
     using Microsoft.OpenApi.Models;
@@ -12,13 +13,86 @@ namespace Boxed.AspNetCore.Swagger.SchemaFilters
     /// <seealso cref="ISchemaFilter" />
     public class ProblemDetailsSchemaFilter : ISchemaFilter
     {
-        private static readonly OpenApiObject ProblemDetails = new OpenApiObject()
+#pragma warning disable
+        private static readonly OpenApiObject Status400ProblemDetails = new OpenApiObject()
         {
-            { "type", new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.4") },
-            { "title", new OpenApiString("Error description") },
-            { "status", new OpenApiInteger(400) },
-            { "traceId", new OpenApiString("00-fbe064c2e2f29548b7f810d792145bee-148546d7edef1143-00") },
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.1"),
+            ["title"] = new OpenApiString("Bad Request"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status400BadRequest),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+            ["errors"] = new OpenApiObject()
+            {
+                ["property1"] = new OpenApiArray()
+                {
+                    new OpenApiString("The property field is required"),
+                },
+            },
         };
+
+        private static readonly OpenApiObject Status401ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7235#section-3.1"),
+            ["title"] = new OpenApiString("Unauthorized"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status401Unauthorized),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status403ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.3"),
+            ["title"] = new OpenApiString("Forbidden"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status403Forbidden),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status404ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.4"),
+            ["title"] = new OpenApiString("Not Found"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status404NotFound),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status406ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.6"),
+            ["title"] = new OpenApiString("Not Acceptable"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status406NotAcceptable),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status409ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.8"),
+            ["title"] = new OpenApiString("Conflict"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status409Conflict),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status415ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.5.13"),
+            ["title"] = new OpenApiString("Unsupported Media Type"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status415UnsupportedMediaType),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status422ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc4918#section-11.2"),
+            ["title"] = new OpenApiString("Unprocessable Entity"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status422UnprocessableEntity),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+
+        private static readonly OpenApiObject Status500ProblemDetails = new OpenApiObject()
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc7231#section-6.6.1"),
+            ["title"] = new OpenApiString("Internal Server Error"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status500InternalServerError),
+            ["traceId"] = new OpenApiString("00-982607166a542147b435be3a847ddd71-fc75498eb9f09d48-00"),
+        };
+#pragma warning restore
 
         /// <inheritdoc/>
         public void Apply(OpenApiSchema schema, SchemaFilterContext context)
@@ -35,8 +109,9 @@ namespace Boxed.AspNetCore.Swagger.SchemaFilters
 
             if (context.ApiModel.Type == typeof(ProblemDetails))
             {
-                schema.Default = ProblemDetails;
-                schema.Example = ProblemDetails;
+                // TODO: Set the default and example based on the status code.
+                // schema.Default = ProblemDetails;
+                // schema.Example = ProblemDetails;
             }
         }
     }
